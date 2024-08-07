46185f522eaa62b4e97e029e35c897655c736f31
Raphael Fleury <raphael.fleury9@gmail.com>
2024-08-07 20:50:31 -0300

    feat: add content to room page and logic to messages

--- src/app.tsx
+++ src/app.tsx
@@ -1,6 +1,7 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom"
 import { CreateRoom } from "./pages/create-room"
 import { Room } from "./pages/room"
+import { Toaster } from "sonner"
 
 const router = createBrowserRouter([
   { path: '/', element: <CreateRoom/> },
@@ -8,5 +9,8 @@ const router = createBrowserRouter([
 ])
 
 export function App() {
-  return <RouterProvider router={router}/>
+  return <>
+    <RouterProvider router={router}/>
+    <Toaster invert richColors/>
+  </>
 }
--- src/pages/room.tsx
+++ src/pages/room.tsx
@@ -1,7 +1,61 @@
+import logo from '../assets/icon.svg'
+import { ArrowRight, Share2 } from 'lucide-react'
 import { useParams } from "react-router-dom"
+import { toast } from 'sonner'
+import { Message } from '../components/message'
 
 export function Room() {
     const { id } = useParams()
 
-    return <h1>Room {id}</h1>
+    function handleShareRoom() {
+        const url = window.location.href.toString()
+        if (navigator.share !== undefined && navigator.canShare()) {
+            navigator.share({url})
+        } else {
+            navigator.clipboard.writeText(url)
+            toast.info('A URL da sala foi copiada para sua área de transferência')
+        }
+    }
+
+    return (
+        <div className="mx-auto max-w-[640px] flex flex-col gap-6 py-10 px-2">
+            <div className="flex items-center gap-3 px-3">
+                <img src={logo} alt="AMA" className='h-5' />
+                <span className='text-sm text-zinc-500 truncate'>
+                    Código da sala: <span className='text-zinc-300'>{id}</span>
+                </span>
+                <button
+                    type='submit' onClick={handleShareRoom}
+                    className='bg-zinc-800 text-zinc-300 px-3 py-1.5 gap-1.5 flex items-center rounded-lg font-medium text-sm transition-colors hover:bg-zinc-700'
+                >
+                    Compartilhar
+                    <Share2 className='size-4'/>
+                </button>
+            </div>
+
+            <div className="h-px w-full bg-zinc-900"/>
+
+            <form
+                className='flex items-center gap-2 bg-zinc-900 p-2 rounded-xl border border-zinc-800 ring-orange-400 ring-offset-2 ring-offset-zinc-950 focus-within:ring-1'
+            >
+                <input
+                    className='flex-1 text-sm bg-transparent mx-2 outline-none placeholder:text-zinc-500 text-zinc-100'
+                    type='text' name='theme' placeholder='Qual a sua pergunta?' autoComplete='off'
+                />
+                <button 
+                    type="submit"
+                    className='ml-auto bg-orange-400 text-orange-950 px-3 py-1.5 gap-1.5 flex items-center rounded-lg font-medium text-sm transition-colors hover:bg-orange-500 '
+                >
+                    Criar Pergunta
+                    <ArrowRight className='size-4'/>
+                </button>
+            </form>
+
+            <ol className='list-decimal list-outside px-3 space-y-8'>
+                <Message text='Pergunta 1' reactionsAmount={1205}/>
+                <Message text='Pergunta 2' reactionsAmount={255} answered/>
+                <Message text='Pergunta 3' reactionsAmount={99}/>
+            </ol>
+        </div>
+    )
 }
