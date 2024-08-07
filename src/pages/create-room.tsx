9516194dcc98954dea70edfe1308dc358a193cc6
Raphael Fleury <raphael.fleury9@gmail.com>
2024-08-07 20:08:51 -0300

    feat: add content to create room page

--- src/pages/create-room.tsx
+++ src/pages/create-room.tsx
@@ -1,3 +1,39 @@
+import logo from '../assets/icon.svg'
+import { useNavigate } from 'react-router-dom'
+import { ArrowRight } from 'lucide-react'
+
 export function CreateRoom() {
-    return <h1>Create Room</h1>
+    const navigate = useNavigate()
+
+    function handleCreateRoom() {
+        navigate('/room/example')
+    }
+
+    return (
+        <main className='h-screen flex items-center justify-center px-4'>
+            <div className='max-w-[450px] flex flex-col gap-6'>
+                <img src={logo} alt="AMA" className='h-10'/>
+                <p className='leading-relaxed text-zinc-300 text-center'>
+                    Crie uma sala pública de AMA (Ask me anything) e priorize as perguntas mais importantes para a comunidade.
+                </p>
+
+                <form
+                    onSubmit={handleCreateRoom}
+                    className='flex items-center gap-2 bg-zinc-900 p-2 rounded-xl border border-zinc-800 ring-orange-400 ring-offset-2 ring-offset-zinc-950 focus-within:ring-1'
+                >
+                    <input
+                        className='flex-1 text-sm bg-transparent mx-2 outline-none placeholder:text-zinc-500 text-zinc-100'
+                        type='text' name='theme' placeholder='Nome da sala' autoComplete='off'
+                    />
+                    <button 
+                        type="submit"
+                        className='bg-orange-400 text-orange-950 px-3 py-1.5 gap-1.5 flex items-center rounded-lg font-medium text-sm transition-colors hover:bg-orange-500 '
+                    >
+                        Criar Sala
+                        <ArrowRight className='size-4'/>
+                    </button>
+                </form>
+            </div>
+        </main>
+    )
 }
